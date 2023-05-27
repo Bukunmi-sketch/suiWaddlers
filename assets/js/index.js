ee02c4780559c8edba38c95680c2fd55abe7f6f3
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-05-27 17:01:07 +0100

    created roadmap details

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,6 +15,156 @@ closebar.onclick=()=>{
     barButton.style.display="block";
 }
 
+
+let answera=document.querySelector('.answera');
+let answerb=document.querySelector('.answerb');
+let answerc=document.querySelector('.answerc');
+let answerd=document.querySelector('.answerd');
+let answere=document.querySelector('.answere');
+let answerf=document.querySelector('.answerf');
+
+let showa=document.querySelector('#showa');
+let showb=document.querySelector('#showb');
+let showc=document.querySelector('#showc');
+let showd=document.querySelector('#showd');
+let showe=document.querySelector('#showe');
+let showf=document.querySelector('#showf');
+// let prev=document.querySelector('#prev');
+
+showa.onclick=()=>{
+    answera.style.display="block";
+    showa.style.display="none"
+ 
+    // console.log('next');
+}
+
+showb.onclick=()=>{
+  answerb.style.display="block";
+  showb.style.display="none"
+
+  // console.log('next');
+}
+
+
+showc.onclick=()=>{
+  answerc.style.display="block";
+  showc.style.display="none"
+
+  // console.log('next');
+}
+
+showd.onclick=()=>{
+  answerd.style.display="block";
+  showd.style.display="none"
+
+  // console.log('next');
+}
+
+showe.onclick=()=>{
+  answere.style.display="block";
+  showe.style.display="none"
+
+  // console.log('next');
+}
+
+showf.onclick=()=>{
+  answerf.style.display="block";
+  showf.style.display="none"
+
+  // console.log('next');
+}
+
+
+// prev.onclick=()=>{
+//     answera.style.zIndex="3";
+//     answerb.style.zIndex="1";
+//     answerc.style.zIndex="2";
+
+//     answera.style.transform="rotate(0deg)";
+//     answerb.style.transform="rotate(15deg)";
+//     answerc.style.transform="rotate(-15deg)";
+ 
+
+//     console.log('prev');
+// }
+
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+//  // showSlides( n);
+// }
+
+
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("author-container");
+//   //  let dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1}   // [1]
+//     if (n < 1) {slideIndex = slides.length}   //[3]
+//     if (n == 0 ) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//      // slides[i].style.display = "none";  
+//     }
+   
+//     // slides[slideIndex].style.transform = "rotate(15deg)"; 
+//     // slides[slideIndex].style.zIndex="3";
+//     slides[slideIndex-1].style.transform = "rotate(0deg)";
+//     slides[slideIndex-1].style.zIndex="3";
+//     slides[slideIndex].style.transform = "rotate(15deg)";
+//     slides[slideIndex].style.zIndex="2"; 
+//     slides[slideIndex+1].style.transform = "rotate(-15deg)";
+//     slides[slideIndex+1].style.zIndex="1"; 
+//  //  slides[slideIndex++].style.transform = "rotate(15deg)";  
+//     console.log(slideIndex,"slideIndex");
+//     console.log(slideIndex-1,"slideIndex=1")
+//     console.log(slideIndex+1,"slideIndex+1")
+   
+//   }
+
+
+
+
+// let transitiona=document.querySelector('#transitiona');
+// let transitionb=document.querySelector('#transitionb');
+// let transitionc=document.querySelector('#transitionc');
+
+// let next=document.querySelector('#next');
+// let prev=document.querySelector('#prev');
+
+// next.onclick=()=>{
+//     transitiona.style.zIndex="1";
+//     transitionb.style.zIndex="3";
+//     transitionc.style.zIndex="1";
+
+//     transitiona.style.transform="rotate(15deg)";
+//     transitionb.style.transform="rotate(-15deg)";
+//     transitionc.style.transform="rotate(0deg)";
+ 
+//     console.log('next');
+// }
+
+
+// prev.onclick=()=>{
+//     transitiona.style.zIndex="3";
+//     transitionb.style.zIndex="1";
+//     transitionc.style.zIndex="2";
+
+//     transitiona.style.transform="rotate(0deg)";
+//     transitionb.style.transform="rotate(15deg)";
+//     transitionc.style.transform="rotate(-15deg)";
+ 
+
+//     console.log('prev');
+// }
+
+
 // let darkmode=document.querySelector('.darkmode');
 // let body=document.querySelector('body');
 // let header=document.querySelector('header');
